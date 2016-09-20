9e20635a3d1e8d84a90d20c228b460768222c40a
Tao Lin <lin.tao.5711y@gmail.com>
2016-09-19 20:14:33 -0400

    problem 1

---
+++ Problem1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int sum;
+
+  int i;
+  for(i=3; i<1000; i++){
+    if( !(i%3) || !(i%5) ){
+      sum += i;
+    }
+  }
+
+  printf("%d",sum);
+  return sum;
+
+}
